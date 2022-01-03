de8380db6261a5d1d0e8514ec9284aeb83229279
Andrea Freeman <afreeman5566@gmail.com>
2022-01-03 09:17:29 -0600

    profile page : images centered and placeholder image added

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -10,8 +10,8 @@
 User.destroy_all
 
 
-nia = User.create(name: "Issa Rae", password: "12345ABC!", username:"test", bio: "Crafty mc crafterson crafting it up.", image: "https://imgur.com/wQMdcRG")
-ben = User.create(name: "Eddy", password: "54321ABC!", username:"test2", bio: "I like pina-coladas and getting caught in the rain", image: "https://imgur.com/5RnsuiY")
+nia = User.create(name: "Issa Rae", password: "12345ABC!", username:"test", bio: "Crafty mc crafterson crafting it up.", image: "https://kdaniellesmedia.com/wp-content/uploads/2018/10/Issa-Rae-to-Star-In-The-New-Rom-Com-American-Princess.jpg")
+ben = User.create(name: "Eddy", password: "54321ABC!", username:"test2", bio: "I like pina-coladas and getting caught in the rain", image: "https://i.imgur.com/5RnsuiY.jpg")
 
 nia.avatar.attach(
     io: File.open("./public/avatars/meet-me.png"),
