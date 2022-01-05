fef6901e97194e75ea8153b8c56c151e08a3c973
Andrea Freeman <afreeman5566@gmail.com>
2022-01-05 00:03:57 -0600

    landing page styled

--- frontend/frontend/src/Components/Landing.js
+++ frontend/frontend/src/Components/Landing.js
@@ -1,5 +1,8 @@
 import {useNavigate} from "react-router-dom"
 import ProjectGenerator from "./ProjectGenerator"
+import yoga from "../images/yoga-gif.gif"
+
+
 
 function Landing(){
 
@@ -22,16 +25,24 @@ function Landing(){
         <h3 className="subheader">Plan to do what you love to do.</h3>
 
         <div className="landingpage-background">      
-        <img className="landingImage" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/34c52563799919.5abc9eb3f3f75.gif" />
+        <img className="landingImage2" src={yoga} />
         <img className="landingImage" src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/6162ae17485889.562e83a6e5f6f.gif" />
         <img className="landingImage" src= "https://www.maorisakai.com/wp-content/uploads/2021/07/rainday-2.gif" />
         <img className="landingImage" src= "https://media2.giphy.com/media/3o7abs0vn1ZKFf74uk/giphy.gif" />
         <img className="landingImage" src= "https://editorial.designtaxi.com/news-gif0809/1.gif" />
-        <img className="landingImage" src= "https://mir-s3-cdn-cf.behance.net/project_modules/disp/e4352817485889.562ba9088a5a3.gif" />
-        <img className="landingImage" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/34c52563799919.5abc9eb3f3f75.gif" />
+        <img className="landingImage2" src= "https://mir-s3-cdn-cf.behance.net/project_modules/disp/e4352817485889.562ba9088a5a3.gif" />
+        <img className="landingImage2" src={yoga} />
         <img className="landingImage" src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/6162ae17485889.562e83a6e5f6f.gif" />
-
-        {/* <img className="landingImage" src= "https://editorial.designtaxi.com/news-gif0809/1.gif" /> */}
+        <img className="landingImage" src= "https://editorial.designtaxi.com/news-gif0809/1.gif" />
+        <img className="landingImage" src= "https://www.maorisakai.com/wp-content/uploads/2021/07/rainday-2.gif" />
+        <img className="landingImage" src= "https://media2.giphy.com/media/3o7abs0vn1ZKFf74uk/giphy.gif" />
+        <img className="landingImage" src= "https://editorial.designtaxi.com/news-gif0809/1.gif" />
+        <img className="landingImage2" src={yoga} />
+        <img className="landingImage2" src= "https://mir-s3-cdn-cf.behance.net/project_modules/disp/e4352817485889.562ba9088a5a3.gif" />
+        <img className="landingImage" src= "https://editorial.designtaxi.com/news-gif0809/1.gif" />
+        <img className="landingImage" src= "https://www.maorisakai.com/wp-content/uploads/2021/07/rainday-2.gif" />
+        <img className="landingImage" src= "https://media2.giphy.com/media/3o7abs0vn1ZKFf74uk/giphy.gif" />
+        <img className="landingImage" src= "https://editorial.designtaxi.com/news-gif0809/1.gif" />
 
         
         {/* <img className="landingImage" src="https://i.pinimg.com/originals/bc/7e/59/bc7e592b890fbcce9795e3c2cd400643.gif" /> */}
